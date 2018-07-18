8e17af7b33fa6e4e464f32818c374b0f973af217
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-07-18 17:55:03 -0300

    End deep search.

--- Project/src/br/com/lopes/estruturas/Profundidade.java
+++ Project/src/br/com/lopes/estruturas/Profundidade.java
@@ -9,36 +9,45 @@ public class Profundidade {
 	private Pilha fronteira;
 	private Cidade origem;
 	private Cidade destino;
+	private boolean achou;
 
 	public static void main(String[] args) {
 		Mapa mapa = new Mapa();
-		Profundidade p = new Profundidade(mapa.getFortaleza(), mapa.getMossoro());
+		Profundidade p = new Profundidade(mapa.getFortaleza(), mapa.getAquiraz());
 		p.buscar();
 	}
 
 	public Profundidade(Cidade origem, Cidade destino) {
 		this.origem = origem;
-		origem.setVisitado(true);
+		this.origem.setVisitado(true);
 
 		this.destino = destino;
 		this.fronteira = new Pilha(20);
 
-		fronteira.empilhar(origem);
+		fronteira.empilhar(this.origem);
+		achou = false;
 	}
 
 	public void buscar() {
 		Cidade topo = fronteira.getTopo();
 		System.out.println("Topo: " + topo.getNome());
 
-		for (Adjacente a : topo.getAdjacentes()) {
-			System.out.println("Verificando se já visitado: " + a.getCidade().getNome());
-			if (!a.getCidade().isVisitado()) {
-				a.getCidade().setVisitado(true);
-				fronteira.empilhar(a.getCidade());
-				buscar();
-			}
-		}
+		if (topo.equals(destino)) {
+			achou = true;
+		} else {
+			for (Adjacente a : topo.getAdjacentes()) {
+				if (!achou) {
+					System.out.println("Verificando se já visitado: " + a.getCidade().getNome());
+					if (!a.getCidade().isVisitado()) {
+						a.getCidade().setVisitado(true);
+						fronteira.empilhar(a.getCidade());
+						buscar(); // Recursivity
+					}
 
+				}
+			}
+
+		}
 		System.out.println("Desempilhou: " + fronteira.desempilhar().getNome());
 
 	}
